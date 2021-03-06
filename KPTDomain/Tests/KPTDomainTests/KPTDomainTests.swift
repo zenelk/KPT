71b2a4829db1eb1c35eaea60e95567adbab4c657
Hunter Lang <zenelk@gmail.com>
2021-03-05 19:28:56 -0700

    Add test for nil, reuse test code

--- KPTDomain/Tests/KPTDomainTests/KPTDomainTests.swift
+++ KPTDomain/Tests/KPTDomainTests/KPTDomainTests.swift
@@ -9,16 +9,24 @@ final class KPTDomainTests: XCTestCase {
 
     static var all = [
         (
-            "test_givenNewTracker_whenSessionAdded_thenNextKanjiIsCorrect",
-            test_givenNewTracker_whenSessionAdded_thenNextKanjiIsCorrect
+            "test_givenNewTracker_whenSessionAdded_thenNextKanjiIsFirstKanjiOfSession",
+            test_givenNewTracker_whenSessionAdded_thenNextKanjiIsFirstKanjiOfSession
+        ),
+        (
+            "test_givenNewTracker_thenNextKanjiIsNil",
+            test_givenNewTracker_thenNextKanjiIsNil
         )
     ]
 
-    func test_givenNewTracker_whenSessionAdded_thenNextKanjiIsCorrect() {
-        let tracker = KanjiPracticeTracker()
+    private let tracker = KanjiPracticeTracker()
 
+    func test_givenNewTracker_whenSessionAdded_thenNextKanjiIsFirstKanjiOfSession() {
         tracker.addSession(Session(date: Date(), kanji: [Constant.goodKanji]))
 
         XCTAssertEqual(tracker.nextKanji, Constant.goodKanji)
+    }
+
+    func test_givenNewTracker_thenNextKanjiIsNil() {
+        XCTAssertNil(tracker.nextKanji)
     }
 }
