cf6167583834b8bea4bb73f32620d6a7a2fc3d22
Hunter Lang <zenelk@gmail.com>
2021-03-07 14:42:59 -0700

    Rename content view, start building the UI, need to use a framework instead of a package to get previews to work

--- KPT/KPT/KPTApp.swift
+++ KPT/KPT/KPTApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct KPTApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
---
+++ KPT/KPT/RootView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
