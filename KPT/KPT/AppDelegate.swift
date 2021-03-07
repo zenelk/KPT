ace69df45e71a1fb595f3a269f3a1d1feb1a4229
Hunter Lang <zenelk@gmail.com>
2021-03-07 15:43:54 -0700

    Change to storyboard UI since SwiftUI just isn't the right tool for text manipulation

---
+++ KPT/KPT/AppDelegate.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) { }
+    func applicationWillTerminate(_ notification: Notification) { }
+}
